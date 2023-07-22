b126731c5431f6b0fb995eb757da14e05c93523f
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-07-21 22:09:55 -0700

    mainMenu function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inquirer = require("inquirer");
 
 const database = require("./lib/database.js");
+const input = require("./lib/input.js");
 
-require("dotenv").config;
+require("dotenv").config();
 
 const db = new database.Database(process.env.DBPASS);
+
+input.mainMenu(db);
