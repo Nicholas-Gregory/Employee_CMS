c4e5cf52c6b85ad8bd3e19ce2772a66686147f76
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-07-21 23:45:33 -0700

    MVP accomplished

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ const inquirer = require("inquirer");
 
 const database = require("./lib/database.js");
 const input = require("./lib/input.js");
+const display = require("./lib/display.js");
 
 require("dotenv").config();
 
-const db = new database.Database(process.env.DBPASS);
+const loop = async (db) => {
+    let quit = false;
+    while (!quit) {
+        let promptResult = await input.mainMenu(db);        
+        switch (promptResult.mainMenu) {
+            case "View All Departments":
+                display.reportDepartments(db);
+                break;
+            case "View All Roles":
+                display.reportRoles(db);
+                break;
+            case "View All Employees":
+                display.reportEmployees(db);
+                break;
+            case "Add a Department":
+                await input.addDepartment(db);
+                break;
+            case "Add a Role":
+                await input.addRole(db);
+                break;
+            case "Add an Employee":
+                await input.addEmployee(db);
+                break;
+            case "Update an Employee's Role":
+                await input.employeeToUpdate(db);
+                break;
+            case "Quit":
+                quit = true;
+        }
+    }
+};
 
-input.mainMenu(db);
+loop(new database.Database(process.env.DBPASS));
